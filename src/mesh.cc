06467258a849083fb659286ab3a845f779004496
Andrew Chambers <andrewrchambers@gmail.com>
2013-10-21 22:53:23 +0100

    Updated mesh to use VAO and read bone data from collada file

--- src/mesh.cc
+++ src/mesh.cc
@@ -25,48 +25,35 @@
 
 namespace handy
 {
+  void Mesh::VertexBoneData::addBoneData(uint boneID, float weight)
+  {
+    uint idsSize = sizeof(ids)/sizeof(ids[0]);
+    for (uint i = 0 ; i < idsSize ; i++) 
+    {
+      if (weights[i] == 0.0) 
+      {
+        ids[i]     = boneID;
+        weights[i] = weight;
+        return;
+      }        
+    }
     
-  Mesh::MeshEntry::MeshEntry()
-  {
-    VB = INVALID_OGL_VALUE;
-    IB = INVALID_OGL_VALUE;
-    numIndices  = 0;
-    materialIndex = INVALID_MATERIAL;
-  };
-
-  Mesh::MeshEntry::~MeshEntry()
-  {
-    if (VB != INVALID_OGL_VALUE)
-    {
-      glDeleteBuffers(1, &VB);
-    }
-
-    if (IB != INVALID_OGL_VALUE)
-    {
-      glDeleteBuffers(1, &IB);
-    }
+    // should never get here - more bones than we have space for
+    assert(0);
   }
 
-  void Mesh::MeshEntry::init(const std::vector<Vertex>& vertices,
-                             const std::vector<unsigned int>& indices)
-  {
-    numIndices = indices.size();
+  Mesh::Mesh() 
+  { 
+    VAO = 0;
+    // Zero buffers
+    memset(buffers, 0, sizeof(buffers));
+    numBones = 0;
+    scene = nullptr;
+  }
 
-    // Vertex buffer
-    glGenBuffers(1, &VB);
-    glBindBuffer(GL_ARRAY_BUFFER, VB);
-    glBufferData(GL_ARRAY_BUFFER, 
-                 sizeof(Vertex) * vertices.size(), 
-                 &vertices[0], 
-                 GL_STATIC_DRAW);
-
-    // Index buffer
-    glGenBuffers(1, &IB);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IB);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, 
-                 sizeof(unsigned int) * numIndices, 
-                 &indices[0], 
-                 GL_STATIC_DRAW);
+  Mesh::~Mesh() 
+  { 
+    clear(); 
   }
 
   void Mesh::clear()
@@ -79,6 +66,15 @@ namespace handy
         textures[i] = nullptr;
       }
     }
+
+    if (buffers[0] != 0) 
+      glDeleteBuffers(NUM_VBs, buffers);
+       
+    if (VAO != 0) 
+    {
+      glDeleteVertexArrays(1, &VAO);
+      VAO = 0;
+    }
   }
 
   bool Mesh::loadMesh(const std::string& filename)
@@ -86,23 +82,38 @@ namespace handy
     // Release the previously loaded mesh (if it exists)
     clear();
     
+    // Create the VAO
+    glGenVertexArrays(1, &VAO);   
+    glBindVertexArray(VAO);
+    
+    // Create the buffers for the vertices attributes
+    glGenBuffers(NUM_VBs, buffers);
     bool ret = false;
-    Assimp::Importer importer;
-    const aiScene* scene = importer.ReadFile(filename, //aiProcessPreset_TargetRealtime_MaxQuality);
+
+    scene = importer.ReadFile(filename,
       aiProcess_Triangulate | aiProcess_GenSmoothNormals | aiProcess_FlipUVs);
     
     if (scene) 
-      ret = initFromScene(scene, filename);
+    {
+      // Copy matrix to GLM style matrix
+      memcpy(&globalInverseTransform, &scene->mRootNode->mTransformation, 
+        16*sizeof(GLfloat));
+      // Maybe not required.
+      globalInverseTransform = glm::transpose(globalInverseTransform);
+      ret = initFromScene(filename);
+    }
     else 
     {
       LOG(ERROR) << "Error parsing " << filename.c_str() << \
         " more details: " << importer.GetErrorString();
     }
 
+    // Unbind the VAO to make sure it isn't modified
+    glBindVertexArray(0);    
     return ret;
   }
-
-  bool Mesh::initFromScene(const aiScene* scene, const std::string& filename)
+  
+  bool Mesh::initFromScene(const std::string& filename)
   {  
     entries.resize(scene->mNumMeshes);
     textures.resize(scene->mNumMaterials);
@@ -110,31 +121,91 @@ namespace handy
     LOG(INFO) << "Number of meshes: " << scene->mNumMeshes;
     LOG(INFO) << "Number of materials: " << scene->mNumMaterials;
 
-    // Initialize the meshes in the scene one by one
-    for (unsigned int i = 0 ; i < entries.size() ; i++) 
+    std::vector<glm::vec3> positions;
+    std::vector<glm::vec3> normals;
+    std::vector<glm::vec2> texCoords;
+    std::vector<VertexBoneData> bones;
+    std::vector<uint> indices;
+       
+    uint numVertices = 0;
+    uint numIndices = 0;
+    
+    // Count the number of vertices and indices
+    for (uint i = 0 ; i < entries.size() ; i++) 
     {
-      const aiMesh* mesh = scene->mMeshes[i];
-      LOG(INFO) << "Initialising mesh: " << mesh->mName.data;
-      initMesh(i, mesh);
+      entries[i].materialIndex = scene->mMeshes[i]->mMaterialIndex;        
+      entries[i].numIndices    = scene->mMeshes[i]->mNumFaces * 3;
+      entries[i].baseVertex    = numVertices;
+      entries[i].baseIndex     = numIndices;
+      
+      numVertices += scene->mMeshes[i]->mNumVertices;
+      numIndices  += entries[i].numIndices;
+    }
+    
+    // Reserve space in the vectors for the vertex attributes and indices
+    positions.reserve(numVertices);
+    normals.reserve(numVertices);
+    texCoords.reserve(numVertices);
+    bones.resize(numVertices);
+    indices.reserve(numIndices);
+        
+    // Initialize the meshes in the scene one by one
+    for (uint i = 0 ; i < entries.size() ; i++) {
+        const aiMesh* mesh = scene->mMeshes[i];
+        initMesh(i, mesh, positions, normals, texCoords, bones, indices);
     }
 
-    return initMaterials(scene, filename);
-  }
+    if (!initMaterials(filename)) {
+        return false;
+    }
 
-  void Mesh::initMesh(unsigned int meshIndex, const aiMesh* mesh)
-  {
-    entries[meshIndex].materialIndex = mesh->mMaterialIndex;
+    // Generate and populate the buffers with vertex attributes and the indices
+  	glBindBuffer(GL_ARRAY_BUFFER, buffers[POS_VB]);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(positions[0]) * positions.size(), 
+        &positions[0], GL_STATIC_DRAW);
+    glEnableVertexAttribArray(POSITION_LOCATION);
+    glVertexAttribPointer(POSITION_LOCATION, 3, GL_FLOAT, GL_FALSE, 0, 0);    
 
-    LOG(INFO) << "Mesh " << meshIndex << " is material: " << mesh->mMaterialIndex;
+    glBindBuffer(GL_ARRAY_BUFFER, buffers[TEXCOORD_VB]);
+	  glBufferData(GL_ARRAY_BUFFER, sizeof(texCoords[0]) * texCoords.size(), 
+	      &texCoords[0], GL_STATIC_DRAW);
+    glEnableVertexAttribArray(TEX_COORD_LOCATION);
+    glVertexAttribPointer(TEX_COORD_LOCATION, 2, GL_FLOAT, GL_FALSE, 0, 0);
+
+   	glBindBuffer(GL_ARRAY_BUFFER, buffers[NORMAL_VB]);
+	  glBufferData(GL_ARRAY_BUFFER, sizeof(normals[0]) * normals.size(), 
+	      &normals[0], GL_STATIC_DRAW);
+    glEnableVertexAttribArray(NORMAL_LOCATION);
+    glVertexAttribPointer(NORMAL_LOCATION, 3, GL_FLOAT, GL_FALSE, 0, 0);
+
+   	glBindBuffer(GL_ARRAY_BUFFER, buffers[BONE_VB]);
+	  glBufferData(GL_ARRAY_BUFFER, sizeof(bones[0]) * bones.size(), 
+	      &bones[0], GL_STATIC_DRAW);
+    glEnableVertexAttribArray(BONE_ID_LOCATION);
+    glVertexAttribIPointer(BONE_ID_LOCATION, 4, GL_INT, 
+        sizeof(VertexBoneData), (const GLvoid*)0);
+    glEnableVertexAttribArray(BONE_WEIGHT_LOCATION);    
+    glVertexAttribPointer(BONE_WEIGHT_LOCATION, 4, GL_FLOAT, GL_FALSE, 
+        sizeof(VertexBoneData), (const GLvoid*)16);
     
-    std::vector<Vertex> vertices;
-    std::vector<unsigned int> indices;
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, buffers[INDEX_BUFFER]);
+	  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices[0]) * indices.size(), 
+	      &indices[0], GL_STATIC_DRAW);
 
+    return glGetError() == GL_NO_ERROR;
+}
+
+
+  void Mesh::initMesh(uint meshIndex,
+                      const aiMesh* mesh,
+                      std::vector<glm::vec3>& positions,
+                      std::vector<glm::vec3>& normals,
+                      std::vector<glm::vec2>& texCoords,
+                      std::vector<VertexBoneData>& bones,
+                      std::vector<uint>& indices)
+  {    
     const aiVector3D zero(0.0f, 0.0f, 0.0f);
-
-    LOG(INFO) << mesh->mNumVertices;
-    google::FlushLogFiles(google::INFO);
-
+    
     for (unsigned int i = 0 ; i < mesh->mNumVertices; i++) 
     {
       const aiVector3D* pos      = &(mesh->mVertices[i]);
@@ -142,26 +213,64 @@ namespace handy
       const aiVector3D* texCoord = mesh->HasTextureCoords(0) ? 
         &(mesh->mTextureCoords[0][i]) : &zero;
 
-      // Construct a Vertex
-      Vertex v(glm::vec3(pos->x, pos->y, pos->z),
-               glm::vec2(texCoord->x, texCoord->y),
-               glm::vec3(normal->x, normal->y, normal->z));
-
-      vertices.push_back(v);
+      positions.push_back(glm::vec3(pos->x, pos->y, pos->z));
+      normals.push_back(glm::vec3(normal->x, normal->y, normal->z));
+      texCoords.push_back(glm::vec2(texCoord->x, texCoord->y));        
     }
-
-    for (unsigned int i = 0 ; i < mesh->mNumFaces ; i++) {
-        const aiFace& face = mesh->mFaces[i];
-        assert(face.mNumIndices == 3);
-        indices.push_back(face.mIndices[0]);
-        indices.push_back(face.mIndices[1]);
-        indices.push_back(face.mIndices[2]);
+    
+    loadBones(meshIndex, mesh, bones);
+    
+    // Populate the index buffer
+    for (uint i = 0 ; i < mesh->mNumFaces ; i++) 
+    {
+      const aiFace& face = mesh->mFaces[i];
+      assert(face.mNumIndices == 3);
+      indices.push_back(face.mIndices[0]);
+      indices.push_back(face.mIndices[1]);
+      indices.push_back(face.mIndices[2]);
     }
-
-    entries[meshIndex].init(vertices, indices);
   }
 
-  bool Mesh::initMaterials(const aiScene* scene, const std::string& filename)
+
+  void Mesh::loadBones(uint meshIndex, 
+                       const aiMesh* mesh, 
+                       std::vector<VertexBoneData>& bones)
+  {
+    for (uint i = 0 ; i < mesh->mNumBones ; i++) 
+    {
+      uint boneIndex = 0;        
+      std::string boneName(mesh->mBones[i]->mName.data);
+      
+      if (boneMapping.find(boneName) == boneMapping.end()) 
+      {
+        // Allocate an index for a new bone
+        boneIndex = numBones;
+        numBones++;            
+        BoneInfo bi;			
+        boneInfo.push_back(bi);
+
+        memcpy(&boneInfo[boneIndex].boneOffset, &mesh->mBones[i]->mOffsetMatrix,
+            16*sizeof(GLfloat));
+
+        // Comment this out for now - different matrix types.  
+        //boneInfo[boneIndex].boneOffset = mesh->mBones[i]->mOffsetMatrix;            
+        // NOTE: MAY REQUIRE TRANSPOSE...
+        boneMapping[boneName] = boneIndex;
+      }
+      else 
+        boneIndex = boneMapping[boneName];
+      
+      for (uint j = 0 ; j < mesh->mBones[i]->mNumWeights ; j++) 
+      {
+        uint vertexID = entries[meshIndex].baseVertex + 
+          mesh->mBones[i]->mWeights[j].mVertexId;
+        float weight  = mesh->mBones[i]->mWeights[j].mWeight;                   
+        bones[vertexID].addBoneData(boneIndex, weight);
+      }
+    }    
+  }
+
+  bool Mesh::initMaterials(const std::string& filename)
   {
     namespace fs = boost::filesystem;
     fs::path f(filename);
@@ -211,29 +320,24 @@ namespace handy
 
   void Mesh::render()
   {
-    glEnableVertexAttribArray(0);
-    glEnableVertexAttribArray(1);
-    glEnableVertexAttribArray(2);
-
-    for (unsigned int i = 0 ; i < entries.size() ; i++) 
+    glBindVertexArray(VAO);
+    
+    for (uint i = 0 ; i < entries.size() ; i++) 
     {
-      glBindBuffer(GL_ARRAY_BUFFER, entries[i].VB);
+      const uint materialIndex = entries[i].materialIndex;
 
-      glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), 0);
-      glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (const GLvoid*)12);
-      glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (const GLvoid*)20);
-
-      glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, entries[i].IB);
-
-      const unsigned int materialIndex = entries[i].materialIndex;
-      if (materialIndex < textures.size() && textures[materialIndex]) 
+      assert(materialIndex < textures.size());
+      
+      if (textures[materialIndex]) 
         textures[materialIndex]->bind(GL_TEXTURE0);
 
-      glDrawElements(GL_TRIANGLES, entries[i].numIndices, GL_UNSIGNED_INT, 0);
+		  glDrawElementsBaseVertex(GL_TRIANGLES, 
+                               entries[i].numIndices, 
+                               GL_UNSIGNED_INT, 
+                               (void*)(sizeof(uint) * entries[i].baseIndex), 
+                               entries[i].baseVertex);
     }
-
-    glDisableVertexAttribArray(0);
-    glDisableVertexAttribArray(1);
-    glDisableVertexAttribArray(2);
+    // Make sure the VAO is not changed from the outside    
+    glBindVertexArray(0);
   }
 };
--- src/mesh.h
+++ src/mesh.h
@@ -48,41 +48,106 @@ namespace handy
   class Mesh
   {
     public:
-      Mesh() 
-      { };
-
-      ~Mesh() 
-      { 
-        clear(); 
-      }
-
+      Mesh();
+      ~Mesh();
       bool loadMesh(const std::string& filename);
       void render();
 
     private:
-      bool initFromScene(const aiScene* scene, const std::string& filename);
-      void initMesh(unsigned int index, const aiMesh* aiMesh);
-      bool initMaterials(const aiScene* scene, const std::string& filename);
-      void clear();
 
       static const int INVALID_MATERIAL = 0xFFFFFFFF;
       static const int INVALID_OGL_VALUE = 0xFFFFFFFF;
+      static const int NUM_BONES_PER_VERTEX = 10;
 
-      struct MeshEntry {
-          MeshEntry();
-          ~MeshEntry();
+      struct BoneInfo
+      {
+        glm::mat4 boneOffset;
+        glm::mat4 finalTransformation;        
 
-          void init(const std::vector<Vertex>& vertices,
-                    const std::vector<unsigned int>& indices);
+        BoneInfo() : boneOffset(0), finalTransformation(0)
+        { }
+      };
+    
+      struct VertexBoneData
+      {        
+        uint ids[NUM_BONES_PER_VERTEX];
+        float weights[NUM_BONES_PER_VERTEX];
 
-          GLuint VB;
-          GLuint IB;
-          unsigned int numIndices;
-          unsigned int materialIndex;
+        VertexBoneData()
+        {
+          reset();
+        };
+        
+        void reset()
+        {
+          memset(ids, 0, sizeof(ids));
+          memset(weights, 0, sizeof(weights));
+        }
+        
+        void addBoneData(uint boneID, float weight);
       };
 
-      // Keep this to support multiple hands
+
+      bool initFromScene(const std::string& filename);
+      void initMesh(unsigned int index, const aiMesh* mesh,
+                    std::vector<glm::vec3>& positions,
+                    std::vector<glm::vec3>& normals,
+                    std::vector<glm::vec2>& texCoords,
+                    std::vector<VertexBoneData>& bones,
+                    std::vector<unsigned int>& indices);
+      void loadBones(uint meshIndex, 
+                     const aiMesh* mesh, 
+                     std::vector<VertexBoneData>& bones);
+      bool initMaterials(const std::string& filename);
+      void clear();
+
+
+      enum VB_TYPES {
+        INDEX_BUFFER = 0,
+        POS_VB = 1,
+        NORMAL_VB = 2,
+        TEXCOORD_VB = 3,
+        BONE_VB = 4,
+        // Cheeky
+        NUM_VBs = 5            
+      };
+
+      enum VB_LOCATIONS {
+        POSITION_LOCATION = 0,
+        TEX_COORD_LOCATION = 1, 
+        NORMAL_LOCATION = 2,
+        BONE_ID_LOCATION = 3,
+        BONE_WEIGHT_LOCATION = 4 
+      };
+
+      // Pointers for the VAO
+      GLuint VAO;
+      GLuint buffers[NUM_VBs];
+
+      struct MeshEntry {
+          MeshEntry()
+          {
+              numIndices    = 0;
+              baseVertex    = 0;
+              baseIndex     = 0;
+              materialIndex = INVALID_MATERIAL;
+          }
+          
+          unsigned int numIndices;
+          unsigned int baseVertex;
+          unsigned int baseIndex;
+          unsigned int materialIndex;
+      };
+      
       std::vector<MeshEntry> entries;
       std::vector<Texture*> textures;
+       
+      std::map<std::string,uint> boneMapping; // maps a bone name to its index
+      uint numBones;
+      std::vector<BoneInfo> boneInfo;
+      glm::mat4 globalInverseTransform;
+
+      const aiScene* scene;
+      Assimp::Importer importer;
   };
 };
