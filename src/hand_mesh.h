82a9737a252e985ead276887a72e4531ff416397
Andrew Chambers <andrewrchambers@gmail.com>
2013-10-23 22:16:31 +0100

    Still working on hand mesh.  Needs some refactoring, maybe add some more functionality to mesh

--- src/hand_mesh.h
+++ src/hand_mesh.h
@@ -26,36 +26,31 @@ namespace handy
         GLOB_ROT_B,
         GLOB_ROT_C,
         GLOB_ROT_D,
-        // Thumb first knuckle
+        // Thumb
         THUMB_1_X,
-        THUMB_1_Y,
-        // Thumb second knuckle
+        THUMB_1_Z,
         THUMB_2_X,
-        // Thumb third knuckle
         THUMB_3_X,
         // Index finger
         INDEX_1_X,
-        INDEX_1_Y,
+        INDEX_1_Z,
         INDEX_2_X,
         INDEX_3_X,
         // Middle finger
         MIDDLE_1_X,
-        MIDDLE_1_Y,
+        MIDDLE_1_Z,
         MIDDLE_2_X,
         MIDDLE_3_X,
         // Ring
         RING_1_X,
-        RING_1_Y,
+        RING_1_Z,
         RING_2_X,
         RING_3_X,
         // Little
         LITTLE_1_X,
-        LITTLE_1_Y,
+        LITTLE_1_Z,
         LITTLE_2_X,
         LITTLE_3_X,
       };
-        
-//      
-//      std::vector<double> pose;
   };
 };
